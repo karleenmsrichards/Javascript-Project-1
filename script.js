1f913b1fe3e99e039fc21a1cae6663d164a013fc
Karleen M S Richards <108957290+karleenmsrichards@users.noreply.github.com>
2022-11-19 14:31:05 +0000

    added button

--- script.js
+++ script.js
@@ -1,11 +1,20 @@
+
 let saveEl = document.getElementById("save-el")
 let countEl = document.getElementById("count-el")
 let count = 0
+let totalCount = 0
+let totalEl = document.getElementById("total-el")
 
 function increment() {
     count += 1
-    countEl.textContent = count
+    countEl.textContent = count  
 }
+
+function total() {  
+    totalCount += 1
+    totalEl.textContent = count
+}
+
 
 function save() {
     let countStr = count + " - "
@@ -13,3 +22,4 @@ function save() {
     countEl.textContent = 0
     count = 0
 }
+
